ff705047259315e2cd78751e6e8cac26dfa5925f
Mikkel <mikkj17@student.sdu.dk>
2020-12-07 10:32:59 +0100

    Another test commit

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,13 +1,11 @@
 from . import tag_requests
-from . import tag_responses
 from .messaging import RabbitMQ
-from typing import Dict
 
 def test() -> None:
     #create_tag("leet", "This is a description", 1337, 420)
     #print(update_tag(1, "Updated name", "Updated description"))
     tags = tag_requests.request_tags_for_post(post_id=1337)
-    print(tag_responses.return_tags_for_post(tags))
+    print(tags)
 
 
 
