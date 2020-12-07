0f92c8516d57acfd8cde1636da97d855223d7505
Mikkel <mikkj17@student.sdu.dk>
2020-12-07 10:37:46 +0100

    Another test commit

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -2,8 +2,6 @@ from . import tag_requests
 from .messaging import RabbitMQ
 
 def test() -> None:
-    #create_tag("leet", "This is a description", 1337, 420)
-    #print(update_tag(1, "Updated name", "Updated description"))
     tags = tag_requests.request_tags_for_post(post_id=1337)
     print(tags)
 
